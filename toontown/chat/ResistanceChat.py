f84500cff9f2613ddc9a1224dfdb75c6e44323bb
Mikey <teku1998@gmail.com>
2015-08-07 17:56:17 -0400

    Removed TOTW Die temp

--- toontown/chat/ResistanceChat.py
+++ toontown/chat/ResistanceChat.py
@@ -20,7 +20,6 @@ RESISTANCE_TOONUP = 0
 RESISTANCE_RESTOCK = 1
 RESISTANCE_MONEY = 2
 RESISTANCE_DANCE = 3
-RESISTANCE_DIE = 4
 allowedResistanceMessages = []
 if config.GetBool('want-resistance-toonup', True):
     allowedResistanceMessages.append(RESISTANCE_TOONUP)
@@ -30,11 +29,9 @@ if config.GetBool('want-resistance-money', True):
     allowedResistanceMessages.append(RESISTANCE_MONEY)
 if config.GetBool('want-resistance-dance', True):
     allowedResistanceMessages.append(RESISTANCE_DANCE)
-if config.GetBool('want-resistance-die', True):
-    allowedResistanceMessages.append(RESISTANCE_DIE)
 resistanceMenu = [
     RESISTANCE_TOONUP, RESISTANCE_RESTOCK, RESISTANCE_MONEY,
-    RESISTANCE_DANCE, RESISTANCE_DIE
+    RESISTANCE_DANCE
 ]
 resistanceDict = {
     RESISTANCE_TOONUP: {
@@ -82,13 +79,6 @@ resistanceDict = {
         'itemText': TTLocalizer.ResistanceDanceItem,
         'chatText': TTLocalizer.ResistanceDanceChat,
         'values': ['Dance'],
-        'items': [0]
-    },
-    RESISTANCE_DIE: {
-        'menuName': TTLocalizer.ResistanceDieMenu,
-        'itemText': TTLocalizer.ResistanceDieItem,
-        'chatText': TTLocalizer.ResistanceDieChat,
-        'values': ['Die'],
         'items': [0]
     }
 }
@@ -220,13 +210,6 @@ def doEffect(textId, speakingToon, nearbyToons):
             toon = base.cr.doId2do.get(toonId)
             if toon and (not toon.ghostMode):
                 toon.setAnimState('victory')
-    elif menuIndex == RESISTANCE_DIE:
-        effect = BattleParticles.loadParticleFile('resistanceEffectSparkle.ptf')
-        fadeColor = VBase4(1, 0.5, .95, .95)
-        for toonId in nearbyToons:
-            toon = base.cr.doId2do.get(toonId)
-            if toon and (not toon.ghostMode):
-                toon.setAnimState('Sad')
     else:
         return
     recolorToons = Parallel()
