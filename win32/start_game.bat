1a776e2f0541d321faa26e0a19eb7eb92cacbc5a
Mikey <teku1998@gmail.com>
2015-09-08 14:10:20 -0400

    Updates to start_game.bat

--- win32/start_game.bat
+++ win32/start_game.bat
@@ -1,5 +1,5 @@
 @echo off 
-title Toontown: The Edge Console
+title Toontown Edge Console
 echo.UPDATING CLIENT THIS MAY TAKE A MINUTE
 cd C:/repos/resources
 git pull origin master --quiet
@@ -8,7 +8,6 @@ cd C:/repos/src
 git pull origin master --quiet
 cls
 echo.DONE UPDATING
-echo.
 echo.
 
 rem Read the contents of PPYTHON_PATH into %PPYTHON_PATH%:
@@ -20,7 +19,7 @@ set /P TTI_GAMESERVER="Gameserver (DEFAULT: toontownedge.mooo.com): " || ^
 set TTI_GAMESERVER=toontownedge.mooo.com
 goto launch
 :replay
-echo.Please press enter to launch the game again!
+echo.Please press enter to launch the game again! (not sufficient for an update)
 pause>nul
 goto launch
 :launch
@@ -29,8 +28,8 @@ set ttiPassword=password
 set TTI_PLAYCOOKIE=%ttiUsername%
 
 echo ===============================
-echo Starting Toontown: The Edge...
-echo ppython: %PPYTHON_PATH%
+echo Starting Toontown Edge...
+rem echo ppython: %PPYTHON_PATH%
 echo Username: %ttiUsername%
 echo Gameserver: %TTI_GAMESERVER%
 echo ===============================
